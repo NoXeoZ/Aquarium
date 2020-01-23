cbf234398e13f8554ed72839744306faf847e45b
NoXeoZ <d.randrianjatovo@gmail.com>
2020-01-23 12:15:28 +0100

    Création du Projet

---
+++ src/test/java/com/upem/Aquarium/AquariumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upem.Aquarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
