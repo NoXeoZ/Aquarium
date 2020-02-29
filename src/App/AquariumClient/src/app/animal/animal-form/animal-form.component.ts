c16f749740caeae57ac29ff920c78d1dfd037c90
NoXeoZ <d.randrianjatovo@gmail.com>
2020-02-29 02:07:59 +0100

    Front+Back add animal (get species,date) WORKING

--- src/App/AquariumClient/src/app/animal/animal-form/animal-form.component.ts
+++ src/App/AquariumClient/src/app/animal/animal-form/animal-form.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {Animal} from "../../model/animal";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AnimalService} from "../../animal/animal.service";
+import {SpecieService} from "../../specie/specie.service";
+import {Observable} from "rxjs";
+import {Specie} from "../../model/specie";
+
+class Species {
+}
 
 @Component({
   selector: 'app-animal-form',
@@ -7,9 +16,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimalFormComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+
+
+  animal: Animal;
+  species: Specie[];
+
+  ngOnInit(): void {
+    this.speciesService.findAll()
+      .subscribe(species => {
+        this.species = species as Specie[]
+      })
   }
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private animalService: AnimalService,
+    private speciesService: SpecieService) {
+    this.animal = new Animal();
+  }
+
+  onSubmit() {
+    this.animalService.save(this.animal).subscribe(result => this.gotoAnimalList());
+  }
+
+  gotoAnimalList() {
+    this.router.navigate(['/animals']).then(r=>console.log("create OK"));
+  }
 }
